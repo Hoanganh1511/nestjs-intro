f214b4a7f71df33902e255a13a4700a6f2e2bcbf
jiminsample1 <jiminsample1@gmail.com>
2024-09-18 22:40:01 +0700

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { DataSource } from 'typeorm';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'nest',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/app/user.entity.ts
@@ -0,0 +1,12 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+@Entity('users')
+export class User {
+  @PrimaryColumn()
+  id: number;
+  @Column()
+  name: string;
+  @Column({ unique: true })
+  email: string;
+  @Column()
+  password: string;
+}
